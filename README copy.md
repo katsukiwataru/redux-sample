acaadab082e37d1f165dfa5230a9d50c1e855299
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-28 04:05:06 +0900

    feat: add envirionment

---
+++ README copy.md
@@ -0,0 +1,16 @@
+# Qiita App TypeScript
+
+- I created this app because qiita cannot search multiple tags at the same time. Also, I created it because I want to know the user who stocks the article.
+- hosting [https://awesome-ramanujan-6d2b7b.netlify.com/](https://awesome-ramanujan-6d2b7b.netlify.com/)
+
+## Available Scripts
+
+### `npm run dev`
+
+Runs the app in the development mode.
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### `npm build`
+
+build command. generate dist directory.
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+const copyRules = [
+  {
+    from: path.resolve(__dirname, 'src/assets'),
+    to: dist,
+  },
+];
+module.exports = {
+  entry: src + '/index.tsx',
+  output: {
+    path: dist,
+    filename: 'js/[name].bundle.js',
+    chunkFilename: 'js/[name].bundle.js',
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  devServer: {
+    open: true,
+    openPage: '',
+    contentBase: dist,
+    watchContentBase: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: 'ts-loader' }],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: ['url-loader?limit=10000', 'img-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin(copyRules),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
